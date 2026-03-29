a662b9c5df4dee30c6c71787e1a33187edf76b62
Sam Miller <supersamm918@gmail.com>
2026-03-28 22:19:14 -0400

    Added more analysis in eda, changed file name poke_analysis -> poke_data_processing to be more clear

---
+++ scripts/poke_data_processing.py
@@ -0,0 +1,41 @@
+
+#sraddha's code (i'm gonna commit it, but still)
+
+
+#imports
+import pandas as pd
+import numpy as np
+
+
+#read in data
+df = pd.read_csv("data/raw/pokemon.csv")
+
+
+#.loc (fire(
+fire_pokemon = df.loc[df["type_1"] == "Fire"]
+
+#.iloc (first 5 rows)
+print("\nFirst 5 rows of the dataset:\n", df.head())
+subset_iloc = df.iloc[0:5, 0:5]
+
+#boolean (high attack)
+high_attack = df[df["attack"] > 100]
+
+print("\nFire-type Pokémon:\n", fire_pokemon[["name", "type_1", "attack"]].head())
+print("\nHigh attack Pokémon:\n", high_attack[["name", "attack"]].head())
+
+
+
+#new column: strong v weak
+df["strength_category"] = np.where(df["total_points"] > 500, "Strong", "Weak")
+
+#apply() for speed
+def speed_category(speed):
+    if speed > 100:
+        return "Fast"
+    else:
+        return "Slow"
+
+df["speed_category"] = df["speed"].apply(speed_category)
+
+print("\nNew columns:\n", df[["name", "total_points", "strength_category", "speed", "speed_category"]].head())
