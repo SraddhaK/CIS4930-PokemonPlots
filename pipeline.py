a9055b31c10745c188e16e90b0ca450cae5ce5ab
supersamm918 <supersamm918@gmail.com>
2026-04-08 22:07:45 -0400

    completed 4.3 in pipeline.py, created piope_line_error.log for error logging

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,14 @@
 #sraddha's code for 4.1/4.2
-
+# sam's code as well for 4.3 (error handling)
 
 import requests
+import logging
+
+logging.basicConfig(
+    filename = "pipeline_error.log",
+    level = logging.ERROR,
+    format = "%(asctime)s - %(levelname)s - %(message)s"
+)
 
 BASE_URL = "https://archive-api.open-meteo.com/v1/archive"
 
@@ -16,34 +23,48 @@ params = {
     "precipitation_unit": "inch"
 }
 
+retry_flag = 2   # 1 to retry (1 time if Timeout exception)
+
+# Error checking, for loop to retry upon Timeout exception
+while retry_flag > 0:
+
+    try:
+        response = requests.get(BASE_URL, params=params, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+    except requests.exceptions.Timeout:
+        retry_flag -= 1     # decrements retry_flag so if this has already retried it will not loop again
+        message = "Request timed out, will skip this run"
+        print(message)
+        logging.error(message)
+    except requests.exceptions.RequestException as e:
+        retry_flag = 0
+        message = f"request error: {e}"
+        print(message)
+        logging.error(message)
+    else:
+        retry_flag = 0
+        if response.status_code == 200:
+            records = []
+
+            hourly = data.get("hourly", {})
+
+            times = hourly.get("time", [])
+            temps = hourly.get("temperature_2m", [])
 
 
-response = requests.get(BASE_URL, params=params, timeout=10)
 
-print("Status:", response.status_code)
+            for i in range(len(times)):
+                record = {
+                    "time": times[i] if i < len(times) else None,
+                    "temperature": temps[i] if i < len(temps) else None,
+                    "latitude": data.get("latitude"),
+                    "longitude": data.get("longitude"),
+                    "timezone": data.get("timezone", "Unknown")
+                }
+                records.append(record)
 
-data = response.json()
-
-records = []
-
-hourly = data.get("hourly", {})
-
-times = hourly.get("time", [])
-temps = hourly.get("temperature_2m", [])
-
-
-
-for i in range(len(times)):
-    record = {
-        "time": times[i] if i < len(times) else None,
-        "temperature": temps[i] if i < len(temps) else None,
-        "latitude": data.get("latitude"),
-        "longitude": data.get("longitude"),
-        "timezone": data.get("timezone", "Unknown")
-    }
-    records.append(record)
-
-print(records[:5])  #prints 1st 5 rows, can be removed if ned be
+            print(records[:5])  #prints 1st 5 rows, can be removed if need be
 
 
 
